c01b885ef73a8749ebb7df58f396a00e847cab51
odito <41185434+odito@users.noreply.github.com>
2019-12-12 18:01:59 +0200

    Add files via upload

---
+++ New folder (2)/indexs.js
@@ -0,0 +1,28 @@
+
+
+let btn =document.querySelector('.btn');
+let Links =document.querySelector('nav ul');
+
+
+// for click event
+
+btn.addEventListener('click', function(){
+ console.log('you clicked the hamburger button');
+ Links.classList.toggle('open');
+
+})
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
